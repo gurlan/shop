a363df10404ef7681392d3f20eb5d78919943ca3
xinggj <xinggj@zhiwyl.com>
2018-10-30 10:01:55 +0800

    merge

---
+++ app/Services/GoodsService.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zwyl
+ * Date: 2018/10/26
+ * Time: 12:18
+ */
+
+namespace App\Services;
+use App\Models\Category;
+
+
+class CategoryService
+{
+    protected  $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * 取得分类列表
+     * @param $pid
+     * @return mixed
+     */
+    public function getList($pid){
+       return $this->category->getList($pid);
+    }
+
+    /**
+     * 分类信息
+     * @param $id
+     * @return mixed
+     */
+    public function categoryInfo($id){
+       return $this->category->categoryInfo($id);
+   }
+
+    public function edit($request){
+        return $this->category->edit($request);
+    }
+
+
+
+}
---
+++ resources/views/console/category/add.blade.php
@@ -0,0 +1,55 @@
+@extends('console.base');
+@section('content')
+<div id="page-wrapper">
+    <div class="graphs">
+        <div class="xs">
+            <h3>Forms</h3>
+            <div class="tab-content">
+                <div class="tab-pane active" id="horizontal-form">
+                    <form class="form-horizontal"  method="post" action="{{route('admin.category.doEdit')}}">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$category->id}}">
+                        <div class="form-group">
+                            <label for="focusedinput" class="col-sm-2 control-label">分类名</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control1" id="focusedinput" placeholder="分类名" name="name" value="{{$category->name}}">
+                            </div>
+                            <div class="col-sm-2">
+                                <p class="help-block">  {{$errors->first('name')}}</p>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="selector1" class="col-sm-2 control-label">上级分类</label>
+                            <div class="col-sm-8">
+                                <select name="selector1" id="selector1" class="form-control1" name="pid">
+                                    @foreach($list as $v)
+                                    <option value="{{$v.id}}">{{$v['name']}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="focusedinput" class="col-sm-2 control-label">排序</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control1" id="focusedinput" placeholder="排序" name="sort" value="{{$category->sort}}">
+                            </div>
+                        </div>
+
+                        <div class="panel-footer">
+                            <div class="row">
+                                <div class="col-sm-8 col-sm-offset-2">
+                                    <button class="btn-success btn">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+    @endsection
